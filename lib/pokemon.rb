eafbde1bbb4c8b72b07a6c9b1686c2648e7d9a7c
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-07 22:36:38 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -22,7 +22,7 @@ class Pokemon
     SQL
     db.execute(sql, id).map do |row|
       binding.pry
-      (self.new(id: row[0], name: row[1], type: row[2])
+      self.new(id: row[0], name: row[1], type: row[2]
     end
   end
 end
