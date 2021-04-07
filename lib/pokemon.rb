b81b0abd2c1eb9f42218f6bb8751a7181bad3888
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-07 22:36:22 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,6 +20,8 @@ class Pokemon
     sql = <<-SQL
     SELECT * FROM pokemon WHERE id = ? LIMIT 1
     SQL
-    db.execute(sql, id).map {|row| self.new(id: row[0], name: row[1], type: row[2], db: nil)}
+    db.execute(sql, id).map do |row| 
+      (self.new(id: row[0], name: row[1], type: row[2])
+    end
   end
 end
